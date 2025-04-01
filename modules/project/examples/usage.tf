51857cdf8436ea9a71867088f0d10d99a086ae93
Polina Malaeva <polina.malaeva@saritasa.com>
2025-04-01 12:01:16 +0700

    test: test multiple sources in usage.tf

--- modules/project/examples/usage.tf
+++ modules/project/examples/usage.tf
@@ -1,3 +1,13 @@
 module "config" {
   source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=modules/project/v1.1.0"
 }
+
+# Example
+module "config" {
+  source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=1.0.0"
+}
+
+# Example
+module "config" {
+  source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=0.0"
+}
