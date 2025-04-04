60aac1b86bac1346b543f9c2a47a6e4e51fd0031
Polina Malaeva <polina.malaeva@saritasa.com>
2025-04-04 13:16:11 +0700

    feat: update configs for test (#17)
    * feat: update configs for test JIRA-1234
    
    * feat: update modules/config tag to modules/config/v0.4.0
    
    * feat: update modules/database tag to modules/database/v0.6.0
    
    * feat: update modules/project tag to modules/project/v1.4.0
    
    ---------
    
    Co-authored-by: github-actions <github-actions@users.noreply.github.com>

--- modules/project/examples/usage.tf
+++ modules/project/examples/usage.tf
@@ -1,13 +1,13 @@
 module "config" {
-  source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=modules/project/v1.3.0"
+  source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=modules/project/v1.4.0"
 }
 
 # Example
 module "config" {
-  source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=modules/project/v1.3.0"
+  source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=modules/project/v1.4.0"
 }
 
 # Example
 module "config" {
-  source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=modules/project/v1.3.0"
+  source = "git@github.com:sowecthal/test-terraform-module-releaser.git//modules/project?ref=modules/project/v1.4.0"
 }
